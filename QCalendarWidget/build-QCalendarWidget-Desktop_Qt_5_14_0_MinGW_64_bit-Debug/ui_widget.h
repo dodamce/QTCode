4f5e312f3049a41b989aced35ec619113c38d356
dodamce <3030949391@qq.com>
2024-07-05 20:41:43 +0800

    QT 日历窗口测试 	new file:   QCalendarWidget/QCalendarWidget/QCalendarWidget.pro 	new file:   QCalendarWidget/QCalendarWidget/QCalendarWidget.pro.user 	new file:   QCalendarWidget/QCalendarWidget/main.cpp 	new file:   QCalendarWidget/QCalendarWidget/widget.cpp 	new file:   QCalendarWidget/QCalendarWidget/widget.h 	new file:   QCalendarWidget/QCalendarWidget/widget.ui 	new file:   QCalendarWidget/build-QCalendarWidget-Desktop_Qt_5_14_0_MinGW_64_bit-Debug/.qmake.stash 	new file:   QCalendarWidget/build-QCalendarWidget-Desktop_Qt_5_14_0_MinGW_64_bit-Debug/Makefile 	new file:   QCalendarWidget/build-QCalendarWidget-Desktop_Qt_5_14_0_MinGW_64_bit-Debug/QCalendarWidget.exe 	new file:   QCalendarWidget/build-QCalendarWidget-Desktop_Qt_5_14_0_MinGW_64_bit-Debug/main.o 	new file:   QCalendarWidget/build-QCalendarWidget-Desktop_Qt_5_14_0_MinGW_64_bit-Debug/moc_predefs.h 	new file:   QCalendarWidget/build-QCalendarWidget-Desktop_Qt_5_14_0_MinGW_64_bit-Debug/moc_widget.cpp 	new file:   QCalendarWidget/build-QCalendarWidget-Desktop_Qt_5_14_0_MinGW_64_bit-Debug/moc_widget.o 	new file:   QCalendarWidget/build-QCalendarWidget-Desktop_Qt_5_14_0_MinGW_64_bit-Debug/ui_widget.h 	new file:   QCalendarWidget/build-QCalendarWidget-Desktop_Qt_5_14_0_MinGW_64_bit-Debug/widget.o

---
+++ QCalendarWidget/build-QCalendarWidget-Desktop_Qt_5_14_0_MinGW_64_bit-Debug/ui_widget.h
@@ -0,0 +1,57 @@
+/********************************************************************************
+** Form generated from reading UI file 'widget.ui'
+**
+** Created by: Qt User Interface Compiler version 5.14.0
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_WIDGET_H
+#define UI_WIDGET_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QCalendarWidget>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_Widget
+{
+public:
+    QCalendarWidget *calendarWidget;
+    QLabel *label;
+
+    void setupUi(QWidget *Widget)
+    {
+        if (Widget->objectName().isEmpty())
+            Widget->setObjectName(QString::fromUtf8("Widget"));
+        Widget->resize(800, 600);
+        calendarWidget = new QCalendarWidget(Widget);
+        calendarWidget->setObjectName(QString::fromUtf8("calendarWidget"));
+        calendarWidget->setGeometry(QRect(20, 80, 561, 511));
+        label = new QLabel(Widget);
+        label->setObjectName(QString::fromUtf8("label"));
+        label->setGeometry(QRect(150, 20, 381, 41));
+
+        retranslateUi(Widget);
+
+        QMetaObject::connectSlotsByName(Widget);
+    } // setupUi
+
+    void retranslateUi(QWidget *Widget)
+    {
+        Widget->setWindowTitle(QCoreApplication::translate("Widget", "Widget", nullptr));
+        label->setText(QString());
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class Widget: public Ui_Widget {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_WIDGET_H
